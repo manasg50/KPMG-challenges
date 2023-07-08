962cb1e71d793aae030d0bacc347028ec94b07a3
manasg50 <42798286+manasg50@users.noreply.github.com>
2023-07-08 20:46:34 +0530

    Update get_metadataMod.py

--- aws-metadata-json-Challenge 2/src/get_metadataMod.py
+++ aws-metadata-json-Challenge 2/src/get_metadataMod.py
@@ -15,29 +15,42 @@ events/
 ...
 So this function will construct the URL, call the url and fetch the metadata for direct end-points.
 'http://169.254.169.254/latest/meta-data/ami-id'
-But for the URL which contain deeper layer of end-point, this function will identify it and traverse deeper into tree and construct the data structure.
+But for the URL which contain deeper layer of end-point, this function will identify it and 
+traverse deeper into tree and construct the data structure.
 'http://169.254.169.254/latest/meta-data/block-device-mappping/'
 ami
 root
 """
+
+
 def traverse_tree(url, data_path):
     # Initializing an empty dictionary.
     metadata = {}
     # The path obtained by previous calls. For the first time the value will be ['meta-data'].
     # We will loop all the paths obtained by calling the url.
     for path in data_path:
+        
         # The new url path is constructed by appending the path to older url.
-        # For example we got 'http://169.254.169.254/latest/meta-data' in first call. When the recursive call is made to this function then the URL will be 'http://169.254.169.254/latest/meta-data and the path will be ami-id. Now the new_url would be 'http://169.254.169.254/latest/meta-data/ami-id
-        new_url = url + path
+        # For example we got 'http://169.254.169.254/latest/meta-data' in first call. 
+        # When the recursive call is made to this function then the URL will be 'http://169.254.169.254/latest/meta-data and the path will be ami-id. Now the new_url would be 'http://169.254.169.254/latest/meta-data/ami-id
+        # new_url = url + path
+        
         # Calling the url
         req = requests.get(new_url)
+        
         # Getting the response out of the request.
         resp = req.text
+        
         # If the obtained path ends with / then that means the end-point can go more deeper level and data can be extracted.
         # For example block-device-mapping/ we know we need to go one more level deeper as it may contain more data.
+        
         if path.endswith("/"):
-            # To create an array out of obtained values. 'http://169.254.169.254/latest/meta-data/block-device-mappping/' will give ['ami','root'].
+            
+            # To create an array out of obtained values. 
+            #'http://169.254.169.254/latest/meta-data/block-device-mappping/' will give ['ami','root'].
+            
             list_of_values = resp.splitlines()
+            
             # Use recursion to go one more level deeper and construct the metadata.
             # For example in 'http://169.254.169.254/latest/meta-data/block-device-mappping/' will give:
             """
